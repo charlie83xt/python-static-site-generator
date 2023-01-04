6bae538c5d6d169d68fbf66d86996b36b99c1379
charlie83xt <65869775+charlie83xt@users.noreply.github.com>
2023-01-04 18:48:15 +0000

    Checking all errors in Parser 5

--- ssg.py
+++ ssg.py
@@ -1,11 +1,12 @@
-from ssg.site import Site, parsers
+from ssg.site import Site
+import ssg.parsers
 import typer
 
 def main(source= "content", dest= "dist"):
     config = {
         "source": source,
         "dest": dest,
-        parsers: [parsers.ResourceParsers()]
+        parsers: [ssg.parsers.ResourceParsers()]
     }
 
     site = Site(**config).build()
