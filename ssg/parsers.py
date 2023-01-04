7c6478619e5dfb8b2c2cd487dac8c294c816dd14
charlie83xt <65869775+charlie83xt@users.noreply.github.com>
2023-01-04 18:47:04 +0000

    Checking all errors in Parser 4

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -30,5 +30,5 @@ class ResourceParser(Parser):
 
     extensions = [".jpg", ".png", ".gif", ".css", ".html"]
 
-    def Parse(self, path: Path, source: Path, dest: Path):
+    def parse(self, path: Path, source: Path, dest: Path):
         super().copy(path, source, dest)
