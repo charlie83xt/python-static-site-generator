7a83f59a0b1d7ea5f80a8d9456e17d197e864e11
charlie83xt <65869775+charlie83xt@users.noreply.github.com>
2023-01-05 12:11:04 +0000

    The parser subclasses 1

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -46,7 +46,7 @@ class MarkdownParser(Parser):
         super().copy(path, source, dest)
         content = Content.load(self.read(path))
         html = markdown(content.body)
-        dest.self.write(html)
+        self.write(path, dest, html)
         sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".append(format(path.name, content)))
 
 
@@ -58,5 +58,5 @@ class ReStructuredTextParser(Parser):
         super().copy(path, source, dest)
         content = Content.load(self.read(path))
         html = publish_parts(content.body, writer_name="html5")
-        dest.self.write(html["html_body"])
+        self.write(path, dest, html["html_body"])
         sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
